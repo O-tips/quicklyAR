2790803b479b29f063235c76248b01b9a21343e0
si-tm <kumomaya@gmail.com>
2024-11-15 17:03:06 +0900

    fix custom types

--- src/app/event/components/custom-types.ts
+++ src/app/event/components/custom-types.ts
@@ -10,9 +10,19 @@ declare module 'react' {
       'device-orientation-permission-ui'?: string;
     };
     "a-assets": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-    "a-asset-item": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-    "a-camera": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+    "a-asset-item": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+      'src'?: string;
+    };
+    "a-camera": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+      'position'?: string;
+    };
     "a-entity": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-    "a-gltf-model": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+    "a-gltf-model": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+      'rotation'?: string;
+      'position'?: string;
+      'scale'?: string;
+      'src'?: string;
+      'visible'?: string;
+    };
   }
 }
