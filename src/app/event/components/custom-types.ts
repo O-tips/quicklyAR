d2f7fd506d790838ab217f6e8aa589495500717a
si-tm <kumomaya@gmail.com>
2024-11-15 16:56:50 +0900

    remove unused variable#

--- src/app/event/components/custom-types.ts
+++ src/app/event/components/custom-types.ts
@@ -2,13 +2,21 @@
 
 import * as React from 'react';
 
-declare module 'react' {
-  interface IntrinsicElements {
-    "a-scene": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-    "a-assets": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-    "a-asset-item": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-    "a-camera": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-    "a-entity": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-    "a-gltf-model": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      "a-scene": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+        'mindar-image'?: string;
+        'color-space'?: string;
+        'renderer'?: string;
+        'vr-mode-ui'?: string;
+        'device-orientation-permission-ui'?: string;
+      };
+      "a-assets": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+      "a-asset-item": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+      "a-camera": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+      "a-entity": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+      "a-gltf-model": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+    }
   }
 }
