e6f31f09ac6c58a2427681730d4901b3122eaf73
si-tm <kumomaya@gmail.com>
2024-11-15 16:58:48 +0900

    remove namespace

--- src/app/event/components/custom-types.ts
+++ src/app/event/components/custom-types.ts
@@ -1,22 +1,18 @@
-// custom-types.d.ts
-
 import * as React from 'react';
 
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      "a-scene": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
-        'mindar-image'?: string;
-        'color-space'?: string;
-        'renderer'?: string;
-        'vr-mode-ui'?: string;
-        'device-orientation-permission-ui'?: string;
-      };
-      "a-assets": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-      "a-asset-item": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-      "a-camera": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-      "a-entity": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-      "a-gltf-model": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-    }
+declare module 'react' {
+  interface IntrinsicElements {
+    "a-scene": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+      'mindar-image'?: string;
+      'color-space'?: string;
+      'renderer'?: string;
+      'vr-mode-ui'?: string;
+      'device-orientation-permission-ui'?: string;
+    };
+    "a-assets": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+    "a-asset-item": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+    "a-camera": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+    "a-entity": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+    "a-gltf-model": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
   }
 }
